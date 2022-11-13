1abd749b97462e9ddd1fdde7c9050fcbb4e767a6
Brian Bullis <brian@bullis.me>
2022-11-13 18:33:09 -0500

    Added a fix for when a space is on the end of an 'Answerer', a new player is made in the STATS

--- anal.py
+++ anal.py
@@ -73,6 +73,9 @@ writers = pd.ExcelFile(SOURCE_FILE).sheet_names
 def read_writer(writer):
     df = pd.read_excel(SOURCE_FILE, writer)
     res = {}
+
+    #strip any spaces so that an extraneous space on an entry doesn't make a new player in the stats
+    df['Answerer'] = df['Answerer'].str.strip()
     
     #get number of questions
     qs = []
